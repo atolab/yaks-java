e6c788ddc826803208d64266af3ae8275715ccf4
Ivan Paez <ivan.paez@adlinktech.com>
2019-08-22 18:02:06 +0200

    added Thr test and travis CI

---
+++ examples/src/main/java/YPutThr.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+import io.zenoh.Vle;
+import is.yaks.Path;
+import is.yaks.StringValue;
+import is.yaks.Value;
+import is.yaks.Workspace;
+import is.yaks.Yaks;
+
+class YPutThr {
+
+    public static void main(String[] args) {
+        String locator = "tcp/127.0.0.1:7447";
+        if (args.length < 1) {
+            System.out.println("USAGE:");
+            System.out.println("\tYPutThr [I|W]<payload-size> [<zenoh-locator>]");
+            System.out.println("\t\tWhere the optional character in front of payload size means:");
+            System.out.println("\t\t\tI : use a non-direct ByteBuffer (created via ByteBuffer.allocate())");
+            System.out.println("\t\t\tW : use a wrapped ByteBuffer (created via ByteBuffer.wrap())");
+            System.out.println("\t\t\tunset : use a direct ByteBuffer");
+            System.exit(-1);
+        }
+
+        java.nio.ByteBuffer data;
+        int len;
+        String lenArg = args[0];
+        if (lenArg.startsWith("I")) {
+            len = Integer.parseInt(lenArg.substring(1));
+            data = java.nio.ByteBuffer.allocate(len+8);
+            System.out.println("Running throughput test for payload of "+len+" bytes from a non-direct ByteBuffer");
+        } else if (lenArg.startsWith("W")) {
+            len = Integer.parseInt(lenArg.substring(1));
+            byte[] array = new byte[len+8+1024];
+            data = java.nio.ByteBuffer.wrap(array, 100, len+8);
+            System.out.println("Running throughput test for payload of "+len+" bytes from a wrapped ByteBuffer");
+        } else {
+            len = Integer.parseInt(lenArg);
+            data = java.nio.ByteBuffer.allocateDirect(len+8);
+            System.out.println("Running throughput test for payload of "+len+" bytes from a direct ByteBuffer");
+        }
+
+        int posInit = data.position();
+        Vle.encode(data, len);
+        for (int i = 0; i < len; ++i) {
+            data.put((byte) (i%10));
+        }
+        data.flip();
+        data.position(posInit);
+
+        try {
+        	String path = "/test/thr";
+
+        	char[] chars = new char[len];
+    		Arrays.fill(chars, 'X');
+
+            Path p = new Path(path);
+
+            Value v = new StringValue(new String(chars));
+
+            System.out.println("Login to "+locator+"...");
+            Yaks y = Yaks.login(locator, null);
+
+            System.out.println("Use Workspace on '/'");
+            Workspace w = y.workspace(new Path("/"));
+
+            System.out.println("Put on "+p+" : "+v.toString().length()+"b");
+
+            while (true) {
+                w.put(p, v);
+            }
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ examples/src/main/java/YSubThr.java
@@ -0,0 +1,66 @@
+import java.util.List;
+
+import is.yaks.Change;
+import is.yaks.Listener;
+import is.yaks.Path;
+import is.yaks.Selector;
+import is.yaks.Workspace;
+import is.yaks.Yaks;
+
+
+class YSubThr {
+
+    private static final long N = 50000;
+
+    private static long count = 0;
+    private static long start;
+    private static long stop;
+
+    public static void printStats(long start, long stop) {
+        float delta = stop - start;
+        float thpt = N / (delta / 1000);
+        System.out.format("%f msgs/sec%n", thpt);
+    }
+
+    private static class Observer implements Listener {
+    	public void onChanges(List<Change> changes) {
+            if (count == 0) {
+                start = System.currentTimeMillis();
+                count ++;
+            } else if (count < N) {
+                count++;
+            } else {
+                stop = System.currentTimeMillis();
+                printStats(start, stop);
+                System.gc();
+                count = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String locator = "tcp/127.0.0.1:7447";
+        if (args.length > 0) {
+            locator = args[0];
+        }
+
+        String s = "/test/thr";
+        try {
+            Selector selector = new Selector(s);
+
+            System.out.println("Login to "+locator+"...");
+            Yaks y = Yaks.login(locator, null);
+
+            System.out.println("Use Workspace on '/'");
+            Workspace w = y.workspace(new Path("/"));
+
+            System.out.println("Subscribe on "+selector);
+            w.subscribe(selector, new Observer());
+
+            Thread.sleep(60000);
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
